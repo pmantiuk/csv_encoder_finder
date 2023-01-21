003c4ae97711a64e6aa1321c88968545e3b8c399
Paweł Mańtiuk <pawel.mantiuk@gmail.com>
2023-01-21 11:20:51 +0100

    dodanie komentarza do pliku, na jego końcu

--- csv_encoder_finder.py
+++ csv_encoder_finder.py
@@ -21,6 +21,7 @@ for x in range(len(codecs)):
         print('I can\'t load data for', codecs[x], '\n')
         print(input('Press any key...'))
 
+# To jest komentarz, którym sprawdzę w jaki sposób korzystać z git'a
 
 
 
